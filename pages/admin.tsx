b50584e0b010f06e56f0b9cad00eadc4367f7da5
AWGbptak <ptak7305@ravens.benedictine.edu>
2022-02-17 10:36:03 -0600

    Update in admin.tsx
    Made tables expandable in admin view

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -6,135 +6,206 @@ import {
     Tbody,
     Tr,
     Th,
-    Td
+    Td,
+    Box
 } from '@chakra-ui/react';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionIcon,
+    AccordionPanel
+} from '@chakra-ui/Accordion'
+
 
 const AdminPage = () => (
     <Container maxW="container.xl" py={20}>
-        <Heading color="red">Business Office</Heading>
-        <Table variant='simple' backgroundColor="gray.200">
-            <Thead>
-                <Tr>
-                    <Th>Student ID</Th>
-                    <Th>Admit Status</Th>
-                    <Th>Last Name</Th>
-                    <Th>First Name</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                <Tr>
-                    <Td>2038452</Td>
-                    <Td>Freshman</Td>
-                    <Td>Smith</Td>
-                    <Td>John</Td>
-                </Tr>
-                <Tr>
-                    <Td>1234567</Td>
-                    <Td>Transfer</Td>
-                    <Td>Ptak</Td>
-                    <Td>Benjamin</Td>
-                </Tr>
-                <Tr>
-                    <Td>7654321</Td>
-                    <Td>Freshman</Td>
-                    <Td>Monares</Td>
-                    <Td>Michael</Td>
-                </Tr>
-            </Tbody>
-        </Table>
-        <Heading color="red" pt={5}>Financial Aid</Heading>
-        <Table variant='simple' backgroundColor="gray.200">
-            <Thead>
-                <Tr>
-                    <Th>Student ID</Th>
-                    <Th>Admit Status</Th>
-                    <Th>Last Name</Th>
-                    <Th>First Name</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                <Tr>
-                    <Td>2038452</Td>
-                    <Td>Freshman</Td>
-                    <Td>Smith</Td>
-                    <Td>John</Td>
-                </Tr>
-                <Tr>
-                    <Td>1234567</Td>
-                    <Td>Transfer</Td>
-                    <Td>Ptak</Td>
-                    <Td>Benjamin</Td>
-                </Tr>
-                <Tr>
-                    <Td>7654321</Td>
-                    <Td>Freshman</Td>
-                    <Td>Monares</Td>
-                    <Td>Michael</Td>
-                </Tr>
-            </Tbody>
-        </Table>
-        <Heading color="red" pt={5}>Nurse's Office</Heading>
-        <Table variant='simple' backgroundColor="gray.200">
-            <Thead>
-                <Tr>
-                    <Th>Student ID</Th>
-                    <Th>Admit Status</Th>
-                    <Th>Last Name</Th>
-                    <Th>First Name</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                <Tr>
-                    <Td>2038452</Td>
-                    <Td>Freshman</Td>
-                    <Td>Smith</Td>
-                    <Td>John</Td>
-                </Tr>
-                <Tr>
-                    <Td>1234567</Td>
-                    <Td>Transfer</Td>
-                    <Td>Ptak</Td>
-                    <Td>Benjamin</Td>
-                </Tr>
-                <Tr>
-                    <Td>7654321</Td>
-                    <Td>Freshman</Td>
-                    <Td>Monares</Td>
-                    <Td>Michael</Td>
-                </Tr>
-            </Tbody>
-        </Table>
-        <Heading color="red" pt={5}>Athletics</Heading>
-        <Table variant='simple' backgroundColor="gray.200">
-            <Thead>
-                <Tr>
-                    <Th>Student ID</Th>
-                    <Th>Admit Status</Th>
-                    <Th>Last Name</Th>
-                    <Th>First Name</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                <Tr>
-                    <Td>2038452</Td>
-                    <Td>Freshman</Td>
-                    <Td>Smith</Td>
-                    <Td>John</Td>
-                </Tr>
-                <Tr>
-                    <Td>1234567</Td>
-                    <Td>Transfer</Td>
-                    <Td>Ptak</Td>
-                    <Td>Benjamin</Td>
-                </Tr>
-                <Tr>
-                    <Td>7654321</Td>
-                    <Td>Freshman</Td>
-                    <Td>Monares</Td>
-                    <Td>Michael</Td>
-                </Tr>
-            </Tbody>
-        </Table>
+        <Accordion defaultIndex={[0]} allowMultiple>
+            <AccordionItem>
+                <Heading>
+                <AccordionButton>
+                    <Box flex ='1' textAlign='left'>
+                    <Heading color='red' pt={5}>
+                        Business Office
+                    </Heading>
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </Heading>
+                <AccordionPanel pb={4}>
+
+                <Table variant='simple' backgroundColor="gray.200">
+                    <Thead>
+                        <Tr>
+                            <Th>Student ID</Th>
+                            <Th>Admit Status</Th>
+                            <Th>Last Name</Th>
+                            <Th>First Name</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>2038452</Td>
+                            <Td>Freshman</Td>
+                            <Td>Smith</Td>
+                            <Td>John</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>1234567</Td>
+                            <Td>Transfer</Td>
+                            <Td>Ptak</Td>
+                            <Td>Benjamin</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>7654321</Td>
+                            <Td>Freshman</Td>
+                            <Td>Monares</Td>
+                            <Td>Michael</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <Heading>
+                <AccordionButton>
+                    <Box flex ='1' textAlign='left'>
+                    <Heading color='red' pt={5}>
+                        Financial Aid
+                    </Heading>
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </Heading>
+                <AccordionPanel pb={4}>
+
+                <Table variant='simple' backgroundColor="gray.200">
+                    <Thead>
+                        <Tr>
+                            <Th>Student ID</Th>
+                            <Th>Admit Status</Th>
+                            <Th>Last Name</Th>
+                            <Th>First Name</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>2038452</Td>
+                            <Td>Freshman</Td>
+                            <Td>Smith</Td>
+                            <Td>John</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>1234567</Td>
+                            <Td>Transfer</Td>
+                            <Td>Ptak</Td>
+                            <Td>Benjamin</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>7654321</Td>
+                            <Td>Freshman</Td>
+                            <Td>Monares</Td>
+                            <Td>Michael</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <Heading>
+                <AccordionButton>
+                    <Box flex ='1' textAlign='left'>
+                    <Heading color='red' pt={5}>
+                        Nurse's Office
+                    </Heading>
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </Heading>
+                <AccordionPanel pb={4}>
+
+                <Table variant='simple' backgroundColor="gray.200">
+                    <Thead>
+                        <Tr>
+                            <Th>Student ID</Th>
+                            <Th>Admit Status</Th>
+                            <Th>Last Name</Th>
+                            <Th>First Name</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>2038452</Td>
+                            <Td>Freshman</Td>
+                            <Td>Smith</Td>
+                            <Td>John</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>1234567</Td>
+                            <Td>Transfer</Td>
+                            <Td>Ptak</Td>
+                            <Td>Benjamin</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>7654321</Td>
+                            <Td>Freshman</Td>
+                            <Td>Monares</Td>
+                            <Td>Michael</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <Heading>
+                <AccordionButton>
+                    <Box flex ='1' textAlign='left'>
+                    <Heading color='red' pt={5}>
+                        Athletics
+                    </Heading>
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </Heading>
+                <AccordionPanel pb={4}>
+
+                <Table variant='simple' backgroundColor="gray.200">
+                    <Thead>
+                        <Tr>
+                            <Th>Student ID</Th>
+                            <Th>Admit Status</Th>
+                            <Th>Last Name</Th>
+                            <Th>First Name</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>2038452</Td>
+                            <Td>Freshman</Td>
+                            <Td>Smith</Td>
+                            <Td>John</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>1234567</Td>
+                            <Td>Transfer</Td>
+                            <Td>Ptak</Td>
+                            <Td>Benjamin</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>7654321</Td>
+                            <Td>Freshman</Td>
+                            <Td>Monares</Td>
+                            <Td>Michael</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+                </AccordionPanel>
+            </AccordionItem>
+        </Accordion>
     </Container>
 );
 
