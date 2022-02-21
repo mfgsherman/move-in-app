5f1f5433fe6150012869752ee66d69bfe1254d4a
mfgsherman <mfgabrielsherman00@gmail.com>
2022-02-21 11:23:09 -0600

    remove extra padding

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -9,33 +9,45 @@ import {
     Td,
     Box
 } from '@chakra-ui/react';
-
 import {
     Accordion,
     AccordionItem,
     AccordionButton,
     AccordionIcon,
     AccordionPanel
-} from '@chakra-ui/Accordion'
-
+} from '@chakra-ui/accordion'
 
 const AdminPage = () => (
-    <Container maxW="container.xl" py={20}>
-        <Accordion allowToggle defaultIndex={[0]} allowMultiple>
+    <Container 
+        maxW="container.xl" 
+        py={20} 
+        px={0}
+    >
+        <Accordion 
+            allowToggle
+            allowMultiple
+        >
             <AccordionItem>
-                <h2>
                 <AccordionButton>
-                    <Box flex ='1' textAlign='left'>
-                    <Heading color='red' pt={5}>
+                    <Box 
+                        flex ='1' 
+                        textAlign='left'
+                    >
+                    <Heading 
+                        color='red' 
+                        pt={5}
+                    >
                         Business Office
                     </Heading>
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
+                <AccordionPanel p={0}>
                     <Box overflowX="auto">
-                        <Table  variant='simple' backgroundColor="gray.200">
+                        <Table 
+                            variant='simple' 
+                            backgroundColor="gray.200"
+                        >
                             <Thead>
                                 <Tr>
                                     <Th>Student ID</Th>
@@ -68,21 +80,27 @@ const AdminPage = () => (
                     </Box>
                 </AccordionPanel>
             </AccordionItem>
-
             <AccordionItem>
-                <Heading>
                 <AccordionButton>
-                    <Box flex ='1' textAlign='left'>
-                    <Heading color='red' pt={5}>
+                    <Box 
+                        flex ='1' 
+                        textAlign='left'
+                    >
+                    <Heading 
+                        color='red' 
+                        pt={5}
+                    >
                         Financial Aid
                     </Heading>
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
-                </Heading>
-                <AccordionPanel pb={4}>
+                <AccordionPanel p={0}>
                     <Box overflowX="auto">
-                        <Table variant='simple' backgroundColor="gray.200">
+                        <Table 
+                            variant='simple' 
+                            backgroundColor="gray.200"
+                        >
                             <Thead>
                                 <Tr>
                                     <Th>Student ID</Th>
@@ -115,21 +133,27 @@ const AdminPage = () => (
                     </Box>
                 </AccordionPanel>
             </AccordionItem>
-
             <AccordionItem>
-                <Heading>
                 <AccordionButton>
-                    <Box flex ='1' textAlign='left'>
-                    <Heading color='red' pt={5}>
-                        Nurse's Office
+                    <Box 
+                        flex ='1' 
+                        textAlign='left'
+                    >
+                    <Heading 
+                        color='red' 
+                        pt={5}
+                    >
+                        Nurse&apos;s Office
                     </Heading>
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
-                </Heading>
-                <AccordionPanel pb={4}>
+                <AccordionPanel p={0}>
                     <Box overflowX="auto">
-                        <Table variant='simple' backgroundColor="gray.200">
+                        <Table 
+                            variant='simple' 
+                            backgroundColor="gray.200"
+                        >
                             <Thead>
                                 <Tr>
                                     <Th>Student ID</Th>
@@ -162,21 +186,27 @@ const AdminPage = () => (
                     </Box>
                 </AccordionPanel>
             </AccordionItem>
-
             <AccordionItem>
-                <Heading>
                 <AccordionButton>
-                    <Box flex ='1' textAlign='left'>
-                    <Heading color='red' pt={5}>
+                    <Box 
+                        flex ='1' 
+                        textAlign='left'
+                    >
+                    <Heading 
+                        color='red' 
+                        pt={5}
+                    >
                         Athletics
                     </Heading>
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
-                </Heading>
-                <AccordionPanel pb={4}>
+                <AccordionPanel p={0}>
                     <Box overflowX="auto">
-                        <Table variant='simple' backgroundColor="gray.200">
+                        <Table 
+                            variant='simple' 
+                            backgroundColor="gray.200"
+                        >
                             <Thead>
                                 <Tr>
                                     <Th>Student ID</Th>
