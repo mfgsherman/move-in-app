729f27e4402c71c970cd5ad7f261af311daa1923
MikeMonares <96020257+MikeMonares@users.noreply.github.com>
2022-02-21 12:05:01 -0600

    Update in Student login (#11)
    * this is a test
    
    I made a junk java file with a test comment
    
    * Student/Admin login UI update
    
    Login screen now has two different forms, one for the student asking for only their ID or last name, and one for Admin asking for both a user name and a password.
    
    * fix redirects
    
    Co-authored-by: mfgsherman <mfgabrielsherman00@gmail.com>

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,15 +13,15 @@ import {
     InputRightElement,
     TabList,
     Tab,
-    Tabs
+    Tabs,    
+    TabPanel,
+    TabPanels
   } from "@chakra-ui/react";
 import Link from "next/link";
 
-  const LoginPage = () => {
+const LoginPage = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const [isStudent, setIsStudent] = useState(true);
-
-    const handleShowClick = () => setShowPassword(!showPassword);
+    const handleShowClick = () => setShowPassword(!showPassword);    
 
     return (
         <Flex
@@ -32,64 +32,86 @@ import Link from "next/link";
             justifyContent="center"
             alignItems="center"
         >
-        <Stack
-            flexDir="column"
-            mb="2"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Avatar bg="red" />
-            <Heading color="red">Move-In Day Checklist</Heading>
-            <Box minW={{ base: "90%", md: "468px" }} backgroundColor="whiteAlpha.900" boxShadow="md">
-                <Tabs>
-                    <TabList>
-                        <Tab onClick={() => setIsStudent(true)}>Student</Tab>
-                        <Tab onClick={() => setIsStudent(false)}>Administrator</Tab>
-                    </TabList>
-                </Tabs>
-                <form>
-                    <Stack
-                    spacing={4}
-                    p="1rem"
-                    >
-                    <FormControl>
-                        <InputGroup>
-                        <InputLeftElement pointerEvents="none"/>
-                        <Input type="username" placeholder="username" />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl>
-                        <InputGroup>
-                        <InputLeftElement
-                            pointerEvents="none"
-                            color="gray.300"
-                        />
-                        <Input
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Password"
-                        />
-                        <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                            {showPassword ? "Hide" : "Show"}
-                            </Button>
-                        </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
-                    <Link {... isStudent ? {href: "/student"} : {href: "/admin"}}>
-                        <Button
-                            borderRadius={0}
-                            type="submit"
-                            variant="solid"
-                            colorScheme="red"
-                            width="full"
-                        >
-                            Login
-                        </Button>
-                    </Link>
-                    </Stack>
-                </form>
-            </Box>
-        </Stack>
+            <Stack
+                flexDir="column"
+                mb="2"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Avatar bg="red" />
+                <Heading color="red">Move-In Day Checklist</Heading>
+                <Box minW={{ base: "90%", md: "468px" }} backgroundColor="whiteAlpha.900" boxShadow="md">
+                    <Tabs>
+                        <TabList>
+                            <Tab>Student</Tab>
+                            <Tab>Administrator</Tab>                        
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <form>                    
+                                    <Stack spacing={4} p="1rem">
+                                    <FormControl>
+                                        <InputGroup>
+                                        <InputLeftElement pointerEvents="none"/>
+                                        <Input type="username" placeholder="Enter ID or Lastname"/>
+                                        </InputGroup>
+                                    </FormControl>
+                                    <Link href={"/student"} passHref>
+                                        <Button
+                                            borderRadius={0}
+                                            type="submit"
+                                            variant="solid"
+                                            colorScheme="red"
+                                            width="full"
+                                            >
+                                            Login
+                                        </Button>
+                                    </Link>
+                                    </Stack>
+                                </form>
+                            </TabPanel>
+                            <TabPanel>
+                                <form>                    
+                                    <Stack spacing={4} p="1rem">
+                                    <FormControl>
+                                        <InputGroup>
+                                        <InputLeftElement pointerEvents="none"/>
+                                        <Input type="username" placeholder="Username"/>
+                                        </InputGroup>
+                                    </FormControl>
+                                    <FormControl>
+                                        <InputGroup>
+                                        <InputLeftElement pointerEvents="none" color="gray.300"/>
+                                        <Input
+                                            type={showPassword ? "text" : "password"}
+                                            placeholder="Password"
+                                            
+                                        />
+                                        <InputRightElement width="4.5rem">
+                                            <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                            {showPassword ? "Hide" : "Show"}
+                                            </Button>
+                                        </InputRightElement>
+                                        </InputGroup>
+                                    </FormControl>
+                                    <Link href={"/admin"} passHref>
+                                        <Button
+                                            borderRadius={0}
+                                            type="submit"
+                                            variant="solid"
+                                            colorScheme="red"
+                                            width="full"
+                                            >
+                                            Login
+                                        </Button>
+                                    </Link>
+                                    </Stack>
+                                </form>
+                            </TabPanel>                    
+                        </TabPanels>  
+                    </Tabs>                      
+                </Box>                        
+            </Stack>
         </Flex>
     );
 };
