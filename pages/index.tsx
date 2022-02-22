8306807ad6a841b83e7123264ad0cebf2e112daf
mfgsherman <45864157+mfgsherman@users.noreply.github.com>
2022-02-22 12:04:20 -0600

    configure and wire up firestore (#13)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import LoginPage from './login'
-// import StudentPage from './student';
-// import AdminPage from './admin';
 
 const IndexPage = () => (
   <LoginPage />
-  // <StudentPage />
-  // <AdminPage />
 );
 
 export default IndexPage;
