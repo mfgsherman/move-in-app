1ec3b92db332b0cee565106cbd7f3195486cbacf
mfgsherman <mfgabrielsherman00@gmail.com>
2022-04-06 20:08:15 -0500

    add student auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,17 @@
-import {useState} from "react";
+import {useState, useEffect, ChangeEvent} from "react";
+import {useRouter} from "next/router";
+import {
+    collection,
+    QueryDocumentSnapshot,
+    DocumentData,
+    getDocs,
+    doc,
+    getDoc,
+    query,
+    where, 
+    limit,
+    documentId
+} from "@firebase/firestore";
 import {
     Flex,
     Heading,
@@ -9,13 +22,48 @@ import {
     InputLeftElement,
     Box,
     Avatar,
-    FormControl
+    FormControl,
+    Alert,
+    AlertIcon,
+    AlertTitle
   } from "@chakra-ui/react";
-import Link from "next/link";
+import {firestore} from '../firebase/initialize';
 
 const LoginPage = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const handleShowClick = () => setShowPassword(!showPassword);    
+    const router = useRouter(); 
+    const [id, setId] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const handleChange = (event: ChangeEvent<any>) => {
+        setId(event.target.value);
+    }
+
+    const authenticate = async (event: ChangeEvent<any>) => {
+        event.preventDefault();
+
+        const studentRef = doc(firestore, 'student-data', id);
+        await getDoc(studentRef)
+            .then((studentSnap) => {
+                if (studentSnap.exists()) {
+                    router.push({
+                        pathname: '/student',
+                        query: {id: id}
+                    });
+                } else {
+                    setErrorMessage('Student ID not found');
+                }
+            })
+            .catch((error) => {
+                setErrorMessage(`${error.code}: ${error.message}`);
+            }) 
+    }
+
+    // useEffect(() => {
+    //     getStudents();
+
+    //     setTimeout(() => {
+    //         setLoading(false);
+    //     }, 2000)
+    // });
 
     return (
         <Flex
@@ -35,28 +83,39 @@ const LoginPage = () => {
                 <Avatar bg="#b30838" />
                 <Heading color= "#b30838" >Move-In Day Checklist</Heading>
                 <Box minW={{ base: "90%", md: "468px" }} backgroundColor="whiteAlpha.900" boxShadow="md">
-                    <form>                    
+                    <form onSubmit={authenticate}>
                         <Stack spacing={4} p="1rem">
-                        <FormControl>
-                            <InputGroup>
-                            <InputLeftElement pointerEvents="none"/>
-                            <Input colorScheme = "#b30838" type="username" placeholder="Enter Student ID"/>
-                            </InputGroup>
-                        </FormControl>
-                        <Link href={"/student"} passHref>
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement pointerEvents="none"/>
+                                    <Input 
+                                        colorScheme = "#b30838"
+                                        onChange={handleChange}
+                                        placeholder="Enter Student ID"
+                                        type="username"
+                                    />
+                                </InputGroup>
+                            </FormControl>
                             <Button
                                 borderRadius={0}
-                                type="submit"
-                                variant="ghost"
                                 color = "#b30838"
+                                // disabled={loading}
+                                onClick={authenticate}
+                                type="button"
+                                variant="ghost"
                                 width="full"
                                 >
                                 Login
                             </Button>
-                        </Link>
                         </Stack>
                     </form>       
-                </Box>                        
+                </Box>  
+                {errorMessage &&
+                    <Alert status='error'>
+                        <AlertIcon />
+                        <AlertTitle>{errorMessage}</AlertTitle>
+                    </Alert>
+                }                      
             </Stack>
         </Flex>
     );
